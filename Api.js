752e41b645386c22ec476554e0b70240dd6f8542
jaredden1 <james.ifit@gmail.com>
2025-01-29 13:06:31 -0500

    call the function with the name()

--- Api.js
+++ Api.js
@@ -1,7 +1,7 @@
 // require axios
 // set up and name the async function
 // set up try/catch and error handling
-// use consol.logs to test and see data from api and error handling
+// add consol.logs to test and see data from api and error handling
 // call the function with the name()
 // test by running node and name of file in terminal
 
@@ -10,8 +10,9 @@ const axios = require("axios");
 const usersApi = async () => {
   try {
     const response = await axios.get("https://reqres.in/api/users/2"); // added dummy users api.
-    console.log('Api response', response.data)
+    console.log("Api response", response.data);
   } catch (error) {
-    console.log('Error fetching response data', error)
+    console.log("Error fetching response data", error);
   }
 };
+usersApi();
