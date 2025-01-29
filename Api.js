43cdecc072d6c3d8812a644f4c4d1074917c9fee
jaredden1 <james.ifit@gmail.com>
2025-01-29 12:46:41 -0500

    require axios

--- Api.js
+++ Api.js
@@ -1,1 +1,11 @@
-//test initial push
+// require axios 
+// set up and name the async function
+// set up try/catch and error handling
+// use consol.logs to test and see data from api and error handling
+// call the function with the name()
+// test by running node and name of file in terminal
+
+const axios = require('axios');
+
+
+
