124ac8872e51df7638ed2daa7316c45d89aafd1b
jaredden1 <james.ifit@gmail.com>
2025-01-29 12:56:55 -0500

    set up try/catch and error handling

--- Api.js
+++ Api.js
@@ -1,13 +1,14 @@
-// require axios 
+// require axios
 // set up and name the async function
 // set up try/catch and error handling
 // use consol.logs to test and see data from api and error handling
 // call the function with the name()
 // test by running node and name of file in terminal
 
-const axios = require('axios');
+const axios = require("axios");
 
 const usersApi = async () => {
-    
-}
-
+  try {
+    const response = await axios.get("https://reqres.in/api/users/2"); // added dummy users api.
+  } catch (error) {}
+};
