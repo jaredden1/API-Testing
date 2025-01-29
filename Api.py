e7e0925b57e956b32189d1ec96d062819ad89b07
jaredden1 <james.ifit@gmail.com>
2025-01-29 15:26:47 -0500

    test passed

--- Api.py
+++ Api.py
@@ -8,7 +8,7 @@
 
 import requests  
 
-url = "https://jsonplaceholder.typicode.com/posts/1"  # added dummy users api.
+url = "https://jsonplaceholder.typicode.com/posts/1"  # posts api.
 
 def fetch_data():
     try:
