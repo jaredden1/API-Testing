87d9c1ba962707ebef7da1c3d1a4ea8f47fb8eb8
jaredden1 <james.ifit@gmail.com>
2025-01-30 13:11:51 -0500

    test passed

--- Api.py
+++ Api.py
@@ -3,7 +3,7 @@
 # Create a function to handle the API request → fetch_user_data()
 # Set up try/except for error handling: HTTP status code: If 200, print the response data, else print error message.
 # Handle exceptions properly to prevent crashes.
-# Call the function and test in the terminal using: python script_name.py
+# Call the function and test in the terminal using: python3 script_name.py
 
 
 import requests  
